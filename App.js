ac9bfe806b92434ef6fde469e4fea2ec003cb328
caiorrs <caiorrs@gmail.com>
2019-07-26 08:47:59 -0300

    Inclusão de easter eggs conforme número de cliques

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
-import { AppRegistry, Button, StyleSheet, Text, View } from 'react-native';
+import { Alert, AppRegistry, Button, StyleSheet, Text, View } from 'react-native';
+
+var counter = 0;
+
+let easterEggs = (counter) => {
+  var text = '';
+  switch(counter){
+    case 7:
+      text = 'This is an easter egg, please continue tapping';
+      Alert.alert('E45t3r 3gg', text);
+      break;
+    
+    case 30:
+      text = "Don't you have to work?";
+      Alert.alert('1337', text);
+      break;
+    
+    case 100:
+      text = 'Yout think there are more easter eggs?';
+      Alert.alert('4all',text);
+      break;
+    
+    case 200:
+      text = 'This is not a clicking game but you have clicked the button 200 times now';
+      Alert.alert("It's clicking time!",text);
+      break;
+    
+    case 1000:
+      text = 'You probably should be doing other things, go party!';
+      Alert.alert("C'mon Barbie, let's go party! (Uh ooh Uh ooh)",text);
+      break;
+    
+    // eu gostaria de fazer as cores mudarem a cada 50ms a partir de count = 1000
+    // mas não quero fazer com sleep, pois isso travará o programa
+    
+    default:
+      break;
+  }
+}
 
 let newColor = () => {
 
   time = Date.now().toString().slice(-1);
+  
   colors = {
+    // cores de
+    // https://coolors.co/e83f6f-e53d00-ffbf00-32936f-2274a5
+    // https://coolors.co/e53d00-f08700-ffbf00-32936f-2274a5
+    // https://coolors.co/ff0000-ff6e00-ffc700-46f900-00a5ff
+
     0: '#2274a5',
     1: '#32936f',
     2: '#ffbf00',
@@ -15,12 +59,10 @@ let newColor = () => {
     7: '#00a5ff',
     8: '#46f900',
     9: '#ff6eaa',
-  };
-
-  // cores de
-  // https://coolors.co/e83f6f-e53d00-ffbf00-32936f-2274a5
-  // https://coolors.co/e53d00-f08700-ffbf00-32936f-2274a5
-  // https://coolors.co/ff0000-ff6e00-ffc700-46f900-00a5ff
+  }
+  
+    counter += 1;
+    easterEggs(counter);
 
   return (colors[time]);
 }
